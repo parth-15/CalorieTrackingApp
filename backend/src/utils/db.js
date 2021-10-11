0acd34626c4cae30f21874c8d778563f4d2c6473
Parth Patel 2 <201501015@daiict.ac.in>
2021-10-11 16:51:07 +0530

    Backend - Add seed admin utility for creating default admin

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,6 +1,27 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
+import User from '../models/user.model';
+
+const seedAdmin = async () => {
+  const adminName = process.env.DEFAULT_ADMIN_NAME;
+  const adminEmail = process.env.DETAULT_ADMIN_EMAIL;
+  const adminPassword = process.env.DEFAULT_ADMIN_PASSWORD;
+  const adminMaxCalories = process.env.DEFAULT_ADMIN_CALORIES;
+  const admin = await User.findOne({ email: adminEmail });
+  if (!admin) {
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
+    const passwordHash = await bcrypt.hash(adminPassword, saltRounds);
+    await User.create({
+      name: adminName,
+      email: adminEmail,
+      password: passwordHash,
+      maxCalories: adminMaxCalories,
+      role: 'admin',
+    });
+  }
+};
+
 const connectDb = async () => {
   await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -10,6 +31,7 @@ const connectDb = async () => {
 
 const applyDatabaseSetup = async () => {
   await connectDb();
+  await seedAdmin();
 };
 
 export default applyDatabaseSetup;
