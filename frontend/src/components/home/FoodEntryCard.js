82cddb89bc7792fbb8e1d5ea0631ea7fb5faadfb
Parth Patel 2 <201501015@daiict.ac.in>
2021-10-13 16:27:40 +0530

    Frontend - Add Home page for user

---
+++ frontend/src/components/home/FoodEntryCard.js
@@ -0,0 +1,55 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import {DateRangeOutlined} from '@material-ui/icons';
+import {Link} from 'react-router-dom';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  date: {
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  actionArea: {
+    padding: 30,
+  },
+});
+
+const getFormattedTime = time => {
+  const hours = parseInt(time / 60);
+  const minutes = time % 60;
+  return hours + ':' + minutes;
+};
+
+export default function FoodEntryCard({id, name, date, time, calories, meal}) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea className={classes.actionArea}>
+        <Typography variant="h5" component="h2">
+          {name}
+        </Typography>
+        <Box className={classes.date}>
+          <DateRangeOutlined color="action" />
+          <Typography color="textSecondary">
+            {`${date} ${getFormattedTime(time)}`}
+          </Typography>
+        </Box>
+        <Typography color="textPrimary">CALORIES: {calories}</Typography>
+        <Box>
+          <Typography>MEAL: {meal && meal.type}</Typography>
+        </Box>
+      </CardActionArea>
+    </Card>
+  );
+}
---
+++ frontend/src/pages/home/Home.js
@@ -0,0 +1,70 @@
+import {Box, Container, makeStyles, Typography} from '@material-ui/core';
+import {useEffect, useState} from 'react';
+import FoodEntryCard from '../../components/home/FoodEntryCard';
+import {getAllFoodEntryOfUser} from '../../dataAccess/foodEntry';
+import {useAuthenticatedUser} from '../../providers/AuthProvider';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles(theme => ({
+  paginationContainer: {display: 'flex', justifyContent: 'center'},
+  title: {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: theme.spacing(12),
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+  const user = useAuthenticatedUser();
+
+  const [foodEntries, setFoodEntries] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    getAllFoodEntryOfUser(user.id, currentPage - 1).then(({data}) => {
+      setFoodEntries(data.rows);
+      setTotalPages(Math.ceil(data.count / data.perPage));
+    });
+  }, [currentPage]);
+
+  const handlePagination = (event, count) => {
+    setCurrentPage(count);
+  };
+  console.log(foodEntries);
+  return (
+    <div>
+      <Container>
+        <Box my={5} className={classes.title}>
+          <Typography component="h2" variant="h4" style={{flexGrow: 1}}>
+            Foodentries
+          </Typography>
+
+          {/* Add three buttons here for user reporting */}
+        </Box>
+        {foodEntries.map(foodEntry => (
+          <Box my={2} key={foodEntry.id}>
+            <FoodEntryCard {...foodEntry} />
+          </Box>
+        ))}
+        {!foodEntries.length && (
+          <Box my={30}>
+            <Typography align="center" color="textSecondary" variant="h6">
+              No Foodentry found
+            </Typography>
+          </Box>
+        )}
+      </Container>
+
+      <Box className={classes.paginationContainer} my={5}>
+        <Pagination
+          count={totalPages}
+          color="primary"
+          onChange={handlePagination}
+          page={currentPage}
+        />
+      </Box>
+    </div>
+  );
+}
