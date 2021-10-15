1d2391495a71cc000d835af5ec8b1064e292093a
Parth Patel 2 <201501015@daiict.ac.in>
2021-10-15 13:36:24 +0530

    Backend - minor bug fix, fetch salt rounds from env file and change prettier config

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -14,21 +14,20 @@ class AuthController {
           error: "User already exists with this email",
         });
       }
+      const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
       const password = generate_token(6);
       req.body.password = await bcrypt.hash(password, saltRounds);
       req.body.token = generate_token(32);
 
       await userService.create({
         ...req.body,
-        role: "user",
+        role: req.body.role || "user",
       });
 
-      res
-        .status(201)
-        .json({
-          success: true,
-          data: { token: req.body.token, password: req.body.password },
-        });
+      res.status(201).json({
+        success: true,
+        data: { token: req.body.token, password: req.body.password },
+      });
     } catch (err) {
       console.error(err);
       res.status(500).json({ success: false, error: "Something went wrong" });
